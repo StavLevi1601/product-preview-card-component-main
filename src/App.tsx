d7c7fdf3f421f4ba33f7c7143398c7af752b0378
Stav Levi <stav.levi@netnut.io>
2024-05-31 08:38:18 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,38 @@
 import "./index.css";
 import imageProductDesktop from "./images/image-product-desktop.jpg";
-import imagePrice from "./images/price.png";
+import cart from "./assets/icon-cart.svg";
 
 export default function App() {
   return (
-    <div className="bg-[#F2EAE2] h-screen w-screen md:pt-[162px] md:px-[420px] md:pb-[188px] py-[28px] px-[16px] ">
-      <div className="flex w-screen justify-start items-start md:flex-row flex-col">
-        <img
-          className="ImageProduct md:w-[300px] md:h-[450px] object-cover h-[270px] w-[343.7px] rounded-xl"
-          src={imageProductDesktop}
-          alt="Product"
-        />
-        <div className="bg-white flex md:h-[450px] md:w-[300px] w-[343px] h-[371px]  justify-start items-start flex-col gap-[20px] pl-[32px] px-[24px] md:pb-[0px] pb-[24px]">
-          <div className="h-[15px] w-[92px] text-[#6C7289] text-base flex justify-start items-start whitespace-nowrap pt-[32px]">
+    <div className="bg-[#F2EAE2] min-h-screen w-screen flex justify-center items-center md:px-[420px] md:pb-[188px] px-[16px]  pb-[28px] pt-[28px] md:pt-28">
+      <div className="bg-white md:h-[480px] md:w-[600px] rounded-2xl flex md:flex-row flex-col md:pb-0 pb-[24px] ">
+        <div className="flex justify-start items-start md:pt-0">
+          <img
+            src={imageProductDesktop}
+            alt="product"
+            className="md:w-[300px] md:h-[480px] md:rounded-l-lg rounded-t-lg md:object-cover object-none w-[343px] h-[240px]"
+          />
+        </div>
+        <div className="flex justify-start pt-[32px] pl-[31.5px] flex-col gap-[20px]">
+          <div className=" text-[#6C7289] flex items-start whitespace-nowrap font-sans text-sm">
             P E R F U M E
           </div>
-          <div className="md:textGabrielleDesk textGabrielleMobile pt-[20px] pr-[2px]">
+          <div className="md:w-[236px] md:h-[96px] w-[295px] h-[64px] font-serif md:text-4xl text-3xl">
             Gabrielle Essence Eau De Parfum
           </div>
-
-          <div className="text-[#6C7289]  w-[280px] h-[92px]">
+          <div className="md:w-[237px] w-[295px] md:pt-[24px] pt-[16px] text-[#6C7289] font-sans ">
             A floral, solar and voluptuous interpretation composed by Olivier
             Polge, Perfumer-Creator for the House of CHANEL.
           </div>
-          <div className="flex flex-row gap-[19px] line-through">
-            <img className="imagePrice " src={imagePrice} alt="Price" />
-            <div className="flex text-[#6C7289]">$169.99</div>
+          <div className="flex flex-row gap-[19px]">
+            <div className="text-[#3D8168] font-serif text-4xl">$149.99</div>
+            <div className="text-[#6C7289] text-sm line-through">$169.99</div>
           </div>
-          <div className="bg-[#1A4032] flex justify-center items-center md:w-[236px] w-[295px] h-[48px] rounded-[14px] md:pb-[16px] pt-[10px] md:pt-0">
-            <div className="flex justify-center items-center">
-              {/* <IconCart /> */}
-              <div className=" text-white w-[236px] h-[48px]  py-[15px] px[62px] m-auto flex justify-center items-center md:pb-[0px] pb-[24px]">
-                Add to cart
-              </div>
-            </div>
+          <div className="flex flex-row">
+            <button className="bg-[#3D8168] text-white md:w-[236px] w-[295px] h-[48px] rounded-lg font-bold flex items-center justify-center">
+              <img src={cart} alt="cart" />
+              <div className="ml-3">Add to Cart</div>
+            </button>
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"], // Use Montserrat as the sans-serif font
+        serif: ["Fraunces", "serif"], // Use Fraunces as the serif font
+      },
+      textShadow: {
+        default: "2px 3px #000000",
+        // add more custom text shadows here
+      },
+    },
   },
   plugins: [],
 };
